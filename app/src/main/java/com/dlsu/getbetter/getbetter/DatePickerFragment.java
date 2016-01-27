bf60449af8b55315255d06a864a7bb4d4b6a94ef
Jan Michael Dayupay <mike.dayupay03@gmail.com>
2016-01-28 00:48:54 +0800

    DatePickerFragment added
    To set date for patient birthday, DatePickerFragment is used.

---
+++ app/src/main/java/com/dlsu/getbetter/getbetter/DatePickerFragment.java
@@ -0,0 +1,43 @@
+package com.dlsu.getbetter.getbetter;
+
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by mikedayupay on 27/01/2016.
+ */
+public class DatePickerFragment extends DialogFragment {
+
+    DatePickerDialog.OnDateSetListener ondateSet;
+    private int year, month, day;
+
+    public DatePickerFragment() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setRetainInstance(true);
+    }
+
+    public void setCallBack(DatePickerDialog.OnDateSetListener ondate) {
+        ondateSet = ondate;
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    public void setArguments(Bundle args) {
+        super.setArguments(args);
+        year = args.getInt("year");
+        month = args.getInt("month");
+        day = args.getInt("day");
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new DatePickerDialog(getActivity(), ondateSet, year, month, day);
+    }
+}
