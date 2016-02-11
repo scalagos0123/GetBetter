30e087324dd32ae5bbf1565805d1e58f1dd0d0e8
Jan Michael Dayupay <mike.dayupay03@gmail.com>
2016-02-11 11:02:50 +0800

    Add Audio recording function
    Use MediaController class for recording audio using the mic and saving
    it on sd card.

--- app/src/main/java/com/dlsu/getbetter/getbetter/RecordHpiFragment.java
+++ app/src/main/java/com/dlsu/getbetter/getbetter/RecordHpiFragment.java
@@ -3,14 +3,13 @@ package com.dlsu.getbetter.getbetter;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
-import android.text.Layout;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -20,16 +19,12 @@ import java.io.IOException;
  */
 public class RecordHpiFragment extends Fragment implements View.OnClickListener {
 
-    private Button nextBtn;
-    private RelativeLayout recordHpiBtn;
-    private static final String LOG_TAG = "AudioRecordTest";
-    private static String mFileName = null;
+    private Button nextBtn, recordBtn, stopRecBtn, playRecBtn;
+    private MediaRecorder hpiRecorder;
+    private String outputFile;
 
-    //private RecordButton mRecordButton = null;
-    private MediaRecorder mRecorder = null;
 
-    //private PlayButton   mPlayButton = null;
-    private MediaPlayer mPlayer = null;
+
 
 
     public RecordHpiFragment() {
@@ -40,6 +35,15 @@ public class RecordHpiFragment extends Fragment implements View.OnClickListener 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";
+
+        hpiRecorder = new MediaRecorder();
+        hpiRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        hpiRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        hpiRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        hpiRecorder.setOutputFile(outputFile);
+
+
     }
 
     @Override
@@ -49,9 +53,17 @@ public class RecordHpiFragment extends Fragment implements View.OnClickListener 
         View rootView = inflater.inflate(R.layout.fragment_record_hpi, container, false);
 
         nextBtn = (Button)rootView.findViewById(R.id.hpi_next_btn);
-        recordHpiBtn = (RelativeLayout)rootView.findViewById(R.id.record_hpi_btn);
+        recordBtn = (Button)rootView.findViewById(R.id.hpi_record_btn);
+        stopRecBtn = (Button)rootView.findViewById(R.id.hpi_stop_record_btn);
+        playRecBtn = (Button)rootView.findViewById(R.id.hpi_play_recorded_btn);
 
-        recordHpiBtn.setOnClickListener(this);
+        stopRecBtn.setEnabled(false);
+        playRecBtn.setEnabled(false);
+
+
+        recordBtn.setOnClickListener(this);
+        stopRecBtn.setOnClickListener(this);
+        playRecBtn.setOnClickListener(this);
         nextBtn.setOnClickListener(this);
 
         return rootView;
@@ -67,64 +79,58 @@ public class RecordHpiFragment extends Fragment implements View.OnClickListener 
             getActivity().getSupportFragmentManager().beginTransaction().addToBackStack(null).commit();
             getActivity().getSupportFragmentManager().beginTransaction().
                     replace(R.id.fragment_container, summaryPageFragment).commit();
-        } else if(id == R.id.record_hpi_btn) {
 
-        }
-    }
+        } else if(id == R.id.hpi_record_btn) {
 
-
-
-    private void onRecord(boolean start) {
-        if (start) {
             try {
-                startRecording();
+
+                hpiRecorder.prepare();
+                hpiRecorder.start();
+            } catch (IllegalStateException e) {
+
+                e.printStackTrace();
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        } else {
-            stopRecording();
+
+            stopRecBtn.setEnabled(true);
+            Toast.makeText(this.getContext(), "Now Recording....", Toast.LENGTH_LONG).show();
+
+        } else if (id == R.id.hpi_stop_record_btn) {
+
+            hpiRecorder.stop();
+            hpiRecorder.release();
+            hpiRecorder = null;
+
+            stopRecBtn.setEnabled(false);
+            playRecBtn.setEnabled(true);
+
+
+        } else if (id == R.id.hpi_play_recorded_btn) {
+
+            MediaPlayer mp = new MediaPlayer();
+
+            try {
+
+                mp.setDataSource(outputFile);
+            } catch (IOException e ) {
+
+                e.printStackTrace();
+
+            }
+
+            try {
+                mp.prepare();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            mp.start();
+
         }
+
     }
 
-    private void onPlay(boolean start) {
-        if (start) {
-            startPlaying();
-        } else {
-            stopPlaying();
-        }
-    }
 
-    private void startPlaying() {
-        mPlayer = new MediaPlayer();
-        try {
-            mPlayer.setDataSource(mFileName);
-            mPlayer.prepare();
-            mPlayer.start();
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
-        }
-    }
-
-    private void stopPlaying() {
-        mPlayer.release();
-        mPlayer = null;
-    }
-
-    private void startRecording() throws IOException {
-        mRecorder = new MediaRecorder();
-        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        mRecorder.setOutputFile(mFileName);
-        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-
-        // mRecorder.prepare();
-
-        mRecorder.start();
-    }
-
-    private void stopRecording() {
-        mRecorder.stop();
-        mRecorder.release();
-        mRecorder = null;
-    }
 }
