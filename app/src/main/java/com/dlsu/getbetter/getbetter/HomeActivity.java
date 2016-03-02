b886ce89fe4c275fb319602897900601a9e51c0d
Jan Michael Dayupay <mike.dayupay03@gmail.com>
2016-03-03 00:50:19 +0800

    health center selection changed to a spinner

--- app/src/main/java/com/dlsu/getbetter/getbetter/HomeActivity.java
+++ app/src/main/java/com/dlsu/getbetter/getbetter/HomeActivity.java
@@ -4,20 +4,32 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.dlsu.getbetter.getbetter.database.DataAdapter;
+import com.dlsu.getbetter.getbetter.objects.HealthCenter;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 
-public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
     private DataAdapter getBetterDb;
     private RecyclerView urgentCaseRecyclerList;
     private RecyclerView addIntCaseRecyclerList;
     private RecyclerView closedCaseRecyclerList;
+    private Spinner healthCenterSpinner;
+    private int healthCenterId;
+    private String healthCenterName;
+    private ArrayList<HealthCenter> healthCenters;
+    private ArrayAdapter<CharSequence> healthCenterAdapter;
 
 
     @Override
@@ -25,16 +37,37 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        //Button changeHealthCenterBtn = (Button)findViewById(R.id.change_health_center_btn);
         urgentCaseRecyclerList = (RecyclerView)findViewById(R.id.urgent_case_records);
         addIntCaseRecyclerList = (RecyclerView)findViewById(R.id.additional_instructions_case_records);
         closedCaseRecyclerList = (RecyclerView)findViewById(R.id.closed_case_records);
+        healthCenterSpinner = (Spinner)findViewById(R.id.health_center_spinner);
+
 
 
         Button viewCreatePatientBtn = (Button)findViewById(R.id.view_create_patient_records_btn);
 
         viewCreatePatientBtn.setOnClickListener(this);
+        //changeHealthCenterBtn.setOnClickListener(this);
+        healthCenterSpinner.setOnItemSelectedListener(this);
 
-        // TODO: 14/02/2016 store health center id in a shared preferences
+
+        healthCenters = new ArrayList<>();
+        initializeDatabase();
+        getHealthCenter();
+        String healthCenterArray [] = new String[healthCenters.size()] ;
+        for(int i = 0; i < healthCenters.size(); i++) {
+
+            healthCenterArray[i] = healthCenters.get(i).getHealthCenterName();
+
+        }
+
+        healthCenterAdapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_dropdown_item, healthCenterArray);
+
+        healthCenterId = healthCenters.get(0).getHealthCenterId();
+        healthCenterName = healthCenters.get(0).getHealthCenterName();
+        healthCenterSpinner.setAdapter(healthCenterAdapter);
+
 
     }
 
@@ -49,11 +82,62 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    public void getHealthCenter() {
+
+        try {
+            getBetterDb.openDatabase();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        healthCenters.addAll(getBetterDb.getHealthCenters());
+
+        getBetterDb.closeDatabase();
+
+    }
+
+    public int getHealthCenterId (String healthCenterName) {
+
+        int id = 0;
+        try {
+            getBetterDb.openDatabase();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        id = getBetterDb.getHealthCenterId(healthCenterName);
+
+        return id;
+    }
+
     @Override
     public void onClick(View v) {
 
-        Intent intent = new Intent(this, ExistingPatientActivity.class);
-        startActivity(intent);
+        int id = v.getId();
 
+        if(id == R.id.view_create_patient_records_btn) {
+
+            Intent intent = new Intent(this, ExistingPatientActivity.class);
+            startActivity(intent);
+        }
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        healthCenterName = parent.getItemAtPosition(position).toString();
+        healthCenterId = getHealthCenterId(healthCenterName);
+        Log.e("health center id", healthCenterId + "");
+        Toast.makeText(HomeActivity.this, healthCenterName + "", Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+        healthCenterName = parent.getSelectedItem().toString();
+        healthCenterId = getHealthCenterId(healthCenterName);
     }
 }
