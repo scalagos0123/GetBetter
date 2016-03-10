c714513398619d2de06adea720176d215f359430
Jan Michael Dayupay <mike.dayupay03@gmail.com>
2016-03-11 07:56:13 +0800

    Add case record list for selected patient
    case record list activity that displays case records of the selected
    patient.
    update selected case record not yet functioning

--- app/src/main/java/com/dlsu/getbetter/getbetter/CreateUpdateCaseRecordActivity.java
+++ app/src/main/java/com/dlsu/getbetter/getbetter/CreateUpdateCaseRecordActivity.java
@@ -1,18 +1,32 @@
 package com.dlsu.getbetter.getbetter;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.dlsu.getbetter.getbetter.adapters.CaseRecordAdapter;
 import com.dlsu.getbetter.getbetter.database.DataAdapter;
+import com.dlsu.getbetter.getbetter.objects.CaseRecord;
+import com.dlsu.getbetter.getbetter.sessionmanagers.NewPatientSessionManager;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class CreateUpdateCaseRecordActivity extends AppCompatActivity implements View.OnClickListener {
 
     private long patientId;
+    private String patientFirstName;
+    private String patientLastName;
     private DataAdapter getBetterDb;
+    private ArrayList<CaseRecord> caseRecords;
+    private int selectedCaseRecordId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,20 +34,47 @@ public class CreateUpdateCaseRecordActivity extends AppCompatActivity implements
         setContentView(R.layout.activity_create_update_case_record);
 
         Bundle extras = getIntent().getExtras();
+        caseRecords = new ArrayList<>();
 
         if(extras != null) {
             patientId = extras.getLong("selectedPatient");
+            patientFirstName = extras.getString("firstname");
+            patientLastName = extras.getString("lastname");
         }
+
+        Log.e("patient id", patientId + "");
+
+        NewPatientSessionManager newPatientSessionManager = new NewPatientSessionManager(this);
+        newPatientSessionManager.setPatientInfo(String.valueOf(patientId), patientFirstName, patientLastName);
+
 
         Button createNewCaseRecBtn = (Button)findViewById(R.id.create_new_case_record_btn);
         Button uploadCaseRecBtn = (Button)findViewById(R.id.upload_case_record);
         Button updateCaseRecBtn = (Button)findViewById(R.id.update_case_record_btn);
+        Button backBtn = (Button)findViewById(R.id.case_records_back_btn);
+        RecyclerView caseRecordsList = (RecyclerView)findViewById(R.id.patient_case_record_list);
+        RecyclerView.LayoutManager caseRecordsLayoutManager = new LinearLayoutManager(this);
+        CaseRecordAdapter caseRecordAdapter = new CaseRecordAdapter(caseRecords);
+
+        initializeDatabase();
+        new GetCaseRecordsTask().execute();
+        Log.e("case records", caseRecords.size() + "");
+
+        caseRecordsList.setHasFixedSize(true);
+        caseRecordsList.setLayoutManager(caseRecordsLayoutManager);
+        caseRecordsList.setAdapter(caseRecordAdapter);
+        caseRecordAdapter.SetOnItemClickListener(new CaseRecordAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+
+                selectedCaseRecordId = caseRecords.get(position).getCaseRecordId();
+            }
+        });
 
         createNewCaseRecBtn.setOnClickListener(this);
         uploadCaseRecBtn.setOnClickListener(this);
         updateCaseRecBtn.setOnClickListener(this);
-
-
+        backBtn.setOnClickListener(this);
     }
 
     private void initializeDatabase () {
@@ -48,6 +89,38 @@ public class CreateUpdateCaseRecordActivity extends AppCompatActivity implements
 
     }
 
+    public void getCaseRecords () {
+
+        try {
+            getBetterDb.openDatabase();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        caseRecords.addAll(getBetterDb.getCaseRecords(patientId));
+
+        getBetterDb.closeDatabase();
+    }
+
+    public void getCaseRecordStatus () {
+
+        try {
+            getBetterDb.openDatabase();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < caseRecords.size(); i++) {
+            ArrayList<String> caseRecordHistory = new ArrayList<>();
+            caseRecordHistory.addAll(getBetterDb.getCaseRecordHistory(caseRecords.get(i).getCaseRecordId()));
+
+            caseRecords.get(i).setCaseRecordUpdatedBy(caseRecordHistory.get(0));
+            caseRecords.get(i).setCaseRecordUpdatedOn(caseRecordHistory.get(1));
+            caseRecords.get(i).setCaseRecordStatus(caseRecordHistory.get(2));
+        }
+
+    }
+
     @Override
     public void onClick(View v) {
 
@@ -55,15 +128,56 @@ public class CreateUpdateCaseRecordActivity extends AppCompatActivity implements
 
         if(id == R.id.create_new_case_record_btn) {
 
+            Intent i = new Intent(this, NewCaseRecordActivity.class);
+            i.putExtra("patientId", patientId);
+            startActivity(i);
+            finish();
+
 
         } else if (id == R.id.update_case_record_btn) {
 
 
         } else if (id == R.id.upload_case_record) {
 
+            Intent i = new Intent(this, UploadCaseRecordToServerActivity.class);
+            startActivity(i);
 
+
+        } else if (id == R.id.case_records_back_btn) {
+
+            finish();
         }
     }
 
+    private class GetCaseRecordsTask extends AsyncTask<Void, Void, Void> {
+
+        private ProgressDialog progressDialog = new ProgressDialog(CreateUpdateCaseRecordActivity.this);
+
+        @Override
+        protected void onPreExecute () {
+            super.onPreExecute();
+            progressDialog.setMessage("Populating Case Records List...");
+            progressDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            getCaseRecords();
+            getCaseRecordStatus();
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute (Void aVoid) {
+
+            super.onPostExecute(aVoid);
+            progressDialog.hide();
+            progressDialog.dismiss();
+        }
+
+    }
 
 }
---
+++ app/src/main/java/com/dlsu/getbetter/getbetter/adapters/CaseRecordAdapter.java
@@ -0,0 +1,101 @@
+package com.dlsu.getbetter.getbetter.adapters;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+import com.dlsu.getbetter.getbetter.R;
+import com.dlsu.getbetter.getbetter.objects.CaseRecord;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mikedayupay on 09/03/2016.
+ */
+public class CaseRecordAdapter extends RecyclerView.Adapter<CaseRecordAdapter.CaseRecordViewHolder> {
+
+    private ArrayList<CaseRecord> caseRecordData;
+    private OnItemClickListener mItemClickListener;
+    private int selectedItem = 0;
+
+
+    public class CaseRecordViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        CardView cardView;
+        TextView controlNumber;
+        TextView chiefComplaint;
+        TextView caseRecordStatus;
+        TextView date;
+
+        public CaseRecordViewHolder (View itemView) {
+            super(itemView);
+
+            cardView = (CardView)itemView.findViewById(R.id.case_record_card_view);
+            controlNumber = (TextView)itemView.findViewById(R.id.case_record_control_number);
+            chiefComplaint = (TextView)itemView.findViewById(R.id.case_record_chief_complaint);
+            caseRecordStatus = (TextView)itemView.findViewById(R.id.case_record_status);
+            date = (TextView)itemView.findViewById(R.id.case_record_date);
+
+            itemView.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+
+            if(mItemClickListener != null) {
+                mItemClickListener.onItemClick(v, getAdapterPosition());
+                notifyItemChanged(selectedItem);
+                selectedItem = getAdapterPosition();
+                notifyItemChanged(selectedItem);
+            }
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    public void SetOnItemClickListener (final OnItemClickListener mItemClickListener) {
+        this.mItemClickListener = mItemClickListener;
+    }
+
+    public CaseRecordAdapter (ArrayList<CaseRecord> caseRecords) {
+        this.caseRecordData = caseRecords;
+    }
+
+    @Override
+    public CaseRecordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.case_record_list_item, parent, false);
+        CaseRecordViewHolder caseRecordViewHolder = new CaseRecordViewHolder(v);
+
+        return caseRecordViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(CaseRecordViewHolder holder, int position) {
+
+        holder.controlNumber.setText(caseRecordData.get(position).getCaseRecordControlNumber());
+        holder.chiefComplaint.setText(caseRecordData.get(position).getCaseRecordComplaint());
+        holder.caseRecordStatus.setText(caseRecordData.get(position).getCaseRecordStatus());
+        holder.date.setText(caseRecordData.get(position).getCaseRecordUpdatedOn());
+
+        holder.itemView.setSelected(selectedItem == position);
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return caseRecordData.size();
+    }
+
+
+
+
+}
