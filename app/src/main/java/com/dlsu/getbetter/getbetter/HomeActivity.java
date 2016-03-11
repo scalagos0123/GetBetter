605192cab8e0ad863b194974ba205944299090e2
Jan Michael Dayupay <mike.dayupay03@gmail.com>
2016-03-11 08:03:39 +0800

    Add health center session id

--- app/src/main/java/com/dlsu/getbetter/getbetter/HomeActivity.java
+++ app/src/main/java/com/dlsu/getbetter/getbetter/HomeActivity.java
@@ -15,6 +15,7 @@ import android.widget.Toast;
 
 import com.dlsu.getbetter.getbetter.database.DataAdapter;
 import com.dlsu.getbetter.getbetter.objects.HealthCenter;
+import com.dlsu.getbetter.getbetter.sessionmanagers.SystemSessionManager;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -23,14 +24,10 @@ import java.util.ArrayList;
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
     private DataAdapter getBetterDb;
-    private RecyclerView urgentCaseRecyclerList;
-    private RecyclerView addIntCaseRecyclerList;
-    private RecyclerView closedCaseRecyclerList;
-    private Spinner healthCenterSpinner;
     private int healthCenterId;
     private String healthCenterName;
     private ArrayList<HealthCenter> healthCenters;
-    private ArrayAdapter<CharSequence> healthCenterAdapter;
+    SystemSessionManager systemSessionManager;
 
 
     @Override
@@ -39,12 +36,12 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_home);
 
         //Button changeHealthCenterBtn = (Button)findViewById(R.id.change_health_center_btn);
-        urgentCaseRecyclerList = (RecyclerView)findViewById(R.id.urgent_case_records);
-        addIntCaseRecyclerList = (RecyclerView)findViewById(R.id.additional_instructions_case_records);
-        closedCaseRecyclerList = (RecyclerView)findViewById(R.id.closed_case_records);
-        healthCenterSpinner = (Spinner)findViewById(R.id.health_center_spinner);
+        RecyclerView urgentCaseRecyclerList = (RecyclerView) findViewById(R.id.urgent_case_records);
+        RecyclerView addIntCaseRecyclerList = (RecyclerView) findViewById(R.id.additional_instructions_case_records);
+        RecyclerView closedCaseRecyclerList = (RecyclerView) findViewById(R.id.closed_case_records);
+        Spinner healthCenterSpinner = (Spinner) findViewById(R.id.health_center_spinner);
 
-
+        systemSessionManager = new SystemSessionManager(this);
 
         Button viewCreatePatientBtn = (Button)findViewById(R.id.view_create_patient_records_btn);
         Button downloadAdditionalContentBtn = (Button)findViewById(R.id.download_content_btn);
@@ -65,13 +62,11 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
         }
 
-        healthCenterAdapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_dropdown_item, healthCenterArray);
+        ArrayAdapter<CharSequence> healthCenterAdapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_dropdown_item, healthCenterArray);
 
         healthCenterId = healthCenters.get(0).getHealthCenterId();
         healthCenterName = healthCenters.get(0).getHealthCenterName();
         healthCenterSpinner.setAdapter(healthCenterAdapter);
-
-
     }
 
     public void initializeDatabase () {
@@ -101,7 +96,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
     public int getHealthCenterId (String healthCenterName) {
 
-        int id = 0;
+        int id;
         try {
             getBetterDb.openDatabase();
         } catch (SQLException e) {
@@ -109,6 +104,8 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         id = getBetterDb.getHealthCenterId(healthCenterName);
+
+        getBetterDb.closeDatabase();
 
         return id;
     }
@@ -120,11 +117,19 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
         if(id == R.id.view_create_patient_records_btn) {
 
+            systemSessionManager.setHealthCenter(healthCenterName, String.valueOf(healthCenterId));
+
             Intent intent = new Intent(this, ExistingPatientActivity.class);
+
             startActivity(intent);
+
         } else if (id == R.id.download_content_btn) {
 
+            systemSessionManager.setHealthCenter(healthCenterName, String.valueOf(healthCenterId));
 
+            Intent i = new Intent(this, DownloadContentActivity.class);
+
+            startActivity(i);
         }
     }
 
