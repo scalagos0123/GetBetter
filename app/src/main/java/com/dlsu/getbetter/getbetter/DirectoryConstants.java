1c27466015ecb23cbea1eca49c9f094b777927d3
Jan Michael Dayupay <mike.dayupay03@gmail.com>
2016-05-13 03:13:57 +0800

    Class containing constants for directories and URLs

---
+++ app/src/main/java/com/dlsu/getbetter/getbetter/DirectoryConstants.java
@@ -0,0 +1,17 @@
+package com.dlsu.getbetter.getbetter;
+
+/**
+ * Created by mikedayupay on 11/05/2016.
+ */
+public class DirectoryConstants {
+
+    public static final String PROFILE_IMAGE_DIRECTORY_NAME = "Profile Images";
+
+    public static final String CASE_RECORD_ATTACHMENT_IMAGE_DIRECTORY_NAME = "Case Record Attachments";
+
+    public static final String UPLOAD_PATIENT_SERVER_SCRIPT_URL = "http://128.199.205.226/get_better/upload_patient.php";
+
+    public static final String UPLOAD_CASE_RECORD_SERVER_SCRIPT_URL = "http://128.199.205.226/get_better/upload_case_record.php";
+
+
+}
