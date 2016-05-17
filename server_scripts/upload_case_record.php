08ec24bd1cde7053ad1d0825c0ad4575d4231205
Jan Michael Dayupay <mike.dayupay03@gmail.com>
2016-05-17 14:23:25 +0800

    Upload Case Record PHP script

--- server_scripts/upload_case_record.php
+++ server_scripts/upload_case_record.php
@@ -8,17 +8,18 @@
     $health_center_id = $_POST['healthCenterId'];
     $complaint = $_POST['complaint'];
     $control_number = $_POST['controlNumber'];
+    $case_id = 6;
 
-    $case_record_status_id = $_POST['caseRecordStatusId'];
-    $updated_by = $_POST['updatedBy'];
-    $updated_on = $_POST['updatedOn'];
+    // $case_record_status_id = $_POST['caseRecordStatusId'];
+    // $updated_by = $_POST['updatedBy'];
+    // $updated_on = $_POST['updatedOn'];
 
 
     if($stmt = $mysqli->prepare("INSERT INTO tbl_case_records
-      (case_record_id, user_id, health_center_id, complaint, control_number)
-      VALUES (?,?,?,?,?)")) {
+      (case_record_id, case_id, user_id, health_center_id, complaint, control_number)
+      VALUES (?,?,?,?,?,?)")) {
 
-        $stmt->bind_param('iiiss', $case_record_id, $user_id, $health_center_id, $complaint, $control_number);
+        $stmt->bind_param('iiiiss', $case_record_id, $case_id, $user_id, $health_center_id, $complaint, $control_number);
 
         $stmt->execute();
 
@@ -32,26 +33,28 @@
          echo "Error Uploading Case Records";
       }
 
-    if($stmt2 = $mysqli->prepare("INSERT INTO tbl_case_record_history
-      (case_record_id, case_record_status_id, updated_by, updated_on) VALUES (?,?,?,?)")) {
-
-        $stmt2->bind_param('iiis', $case_record_id, $case_record_status_id, $updated_by, $updated_on);
-
-        $stmt2->execute();
-
-        $stmt2->close();
-
-
-    } else {
-      echo "Error Uploading Case Record History";
-    }
+    // if($stmt2 = $mysqli->prepare("INSERT INTO tbl_case_record_history
+    //   (case_record_id, case_record_status_id, updated_by, updated_on) VALUES (?,?,?,?)")) {
+    //
+    //     $stmt2->bind_param('iiis', $case_record_id, $case_record_status_id, $updated_by, $updated_on);
+    //
+    //     $stmt2->execute();
+    //
+    //     $stmt2->close();
+    //
+    //
+    // } else {
+    //   echo "Error Uploading Case Record History";
+    // }
 
     echo "Successfully Uploaded Case Record";
+
+  $mysqli->close();
 
   } else {
     echo "Unable to process request!";
   }
 
-$mysqli->close()
+
 
 ?>
