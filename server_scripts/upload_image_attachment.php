d2bb8b36908779b35a2d768bfcaa3fff3993ea6d
Jan Michael Dayupay <mike.dayupay03@gmail.com>
2016-05-17 14:23:17 +0800

    Upload Image attachment PHP Script

---
+++ server_scripts/upload_image_attachment.php
@@ -0,0 +1,46 @@
+<?php
+
+  if (($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['caseRecordId'])) {
+    require_once('db_connect.php');
+
+    $case_record_id = $_POST['caseRecordId'];
+    $attachment_description = $_POST['description'];
+    $attachment_name = $_POST['attachment_name'];
+    $encoded_image = $_POST['encoded_image'];
+    $attachment_type = $_POST['attachment_type'];
+    $uploaded_on = $_POST['uploaded_on'];
+
+    $file_path = "uploads/";
+    $full_path = "/var/www/html/getbetter/" . $file_path;
+    $attachment_path = $full_path . $attachment_name;
+
+    $decoded_image = base64_decode($encoded_image);
+
+    if(file_put_contents($attachment_path, $decoded_image) != false) {
+
+      if($stmt = $mysqli->prepare('INSERT INTO tbl_case_record_attachments
+        (case_record_id, description, case_file_url, case_record_attachment_type_id, uploaded_on)
+        VALUES (?,?,?,?,?)')) {
+
+          $stmt->bind_param('issis', $case_record_id, $attachment_description,
+          $attachment_name, $attachment_type, $uploaded_on);
+
+          $stmt->execute();
+
+          $stmt->close();
+      }
+      echo 'Successfully Uploaded';
+    } else {
+      echo 'Failed to upload attachment';
+    }
+$mysqli->close();
+  }else {
+
+    echo "Unable to process request!";
+  }
+
+
+
+
+
+?>
