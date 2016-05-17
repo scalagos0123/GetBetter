46d028efc9f71adf7a7beca3c92b7637d630cb38
Jan Michael Dayupay <mike.dayupay03@gmail.com>
2016-05-17 14:25:42 +0800

    Download data activity
    Download data list of updated case records added to the app.
    not yet complete for it is lacking the downloading of the whole data.

--- app/src/main/java/com/dlsu/getbetter/getbetter/DownloadContentActivity.java
+++ app/src/main/java/com/dlsu/getbetter/getbetter/DownloadContentActivity.java
@@ -1,18 +1,44 @@
 package com.dlsu.getbetter.getbetter;
 
+import android.content.res.Resources;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.dlsu.getbetter.getbetter.adapters.CaseRecordDownloadAdapter;
+import com.dlsu.getbetter.getbetter.adapters.CaseRecordUploadAdapter;
 import com.dlsu.getbetter.getbetter.database.DataAdapter;
+import com.dlsu.getbetter.getbetter.objects.CaseRecord;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class DownloadContentActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG_CASE_RECORD = "case_records";
+    private static final String TAG_CASE_RECORD_ID = "case_record_id";
+    private static final String TAG_USER_ID = "user_id";
+    private static final String TAG_COMPLAINT = "complaint";
+    private static final String TAG_HEALTH_CENTER_ID = "health_center_id";
+    private static final String TAG_RECORD_STATUS_ID = "record_status_id";
+    private static final String TAG_UPDATED_ON = "updated_on";
+
+    String myJSON;
+    JSONArray caseRecords = null;
+    ArrayList<CaseRecord> caseRecordsData;
+
     DataAdapter getBetterDb;
+    CaseRecordDownloadAdapter caseRecordDownloadAdapter = null;
+    ListView caseRecordList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +46,14 @@ public class DownloadContentActivity extends AppCompatActivity implements View.O
         setContentView(R.layout.activity_download_content);
 
         Button backBtn = (Button)findViewById(R.id.download_back_btn);
-        ListView caseRecordList = (ListView)findViewById(R.id.download_page_case_record_list);
+        caseRecordList = (ListView)findViewById(R.id.download_page_case_record_list);
 
         backBtn.setOnClickListener(this);
 
+        caseRecordsData = new ArrayList<>();
+
         initializeDatabase();
+        getDownloadableData();
     }
 
     private void initializeDatabase () {
@@ -46,4 +75,129 @@ public class DownloadContentActivity extends AppCompatActivity implements View.O
             finish();
         }
     }
+
+    public void getDownloadableData() {
+
+        class GetCaseRecordList extends AsyncTask<String, Void, String> {
+
+            RequestHandler rh = new RequestHandler();
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                String result = null;
+
+                result = rh.getPostRequest(DirectoryConstants.DOWNLOAD_CASE_RECORD_LIST_URL);
+
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                myJSON = s;
+                populateCaseRecordsList();
+            }
+        }
+
+        GetCaseRecordList getCaseRecordList = new GetCaseRecordList();
+        getCaseRecordList.execute();
+
+    }
+
+    public void populateCaseRecordsList() {
+
+        try {
+            JSONObject jsonObject = new JSONObject(myJSON);
+            caseRecords = jsonObject.getJSONArray(TAG_CASE_RECORD);
+
+            for(int i = 0; i < caseRecords.length(); i++) {
+                JSONObject c = caseRecords.getJSONObject(i);
+                int caseRecordId = Integer.parseInt(c.getString(TAG_CASE_RECORD_ID));
+                String patientName = getUserName(Integer.parseInt(c.getString(TAG_USER_ID)));
+//                String patientName = c.getString(TAG_USER_ID);
+                String complaint = c.getString(TAG_COMPLAINT);
+                String healthCenter = getHealthCenterName(Integer.parseInt(c.getString(TAG_HEALTH_CENTER_ID)));
+                String recordStatus = getCaseRecordStatusString(Integer.parseInt(c.getString(TAG_RECORD_STATUS_ID)));
+                String updatedOn = c.getString(TAG_UPDATED_ON);
+
+                CaseRecord caseRecord = new CaseRecord(caseRecordId, patientName, complaint,
+                        healthCenter, recordStatus, updatedOn);
+                caseRecordsData.add(caseRecord);
+
+            }
+
+            caseRecordDownloadAdapter = new CaseRecordDownloadAdapter(this, R.layout.case_record_item_checkbox, caseRecordsData);
+            caseRecordList.setAdapter(caseRecordDownloadAdapter);
+
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getCaseRecordStatusString(int caseRecordStatusId) {
+
+        String result = null;
+        Resources res = getResources();
+        String[] caseRecordStatuses = res.getStringArray(R.array.record_status);
+
+        switch (caseRecordStatusId) {
+
+            case 1: result = caseRecordStatuses[0];
+                break;
+
+            case 2: result = caseRecordStatuses[1];
+                break;
+
+            case 3: result = caseRecordStatuses[2];
+                break;
+
+            case 4: result = caseRecordStatuses[3];
+                break;
+
+            case 5: result = caseRecordStatuses[4];
+                break;
+
+            case 6: result = caseRecordStatuses[5];
+                break;
+
+            case 7: result = caseRecordStatuses[6];
+                break;
+
+        }
+
+        return result;
+    }
+
+    public String getUserName (int userId) {
+
+        try {
+            getBetterDb.openDatabase();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        String result;
+        result = getBetterDb.getPatientName(userId);
+
+        getBetterDb.closeDatabase();
+
+        return result;
+    }
+
+    public String getHealthCenterName (int healthCenterId) {
+
+        try {
+            getBetterDb.openDatabase();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        String result;
+        result = getBetterDb.getHealthCenterName(healthCenterId);
+
+        getBetterDb.closeDatabase();
+
+        return result;
+    }
+
 }
---
+++ app/src/main/java/com/dlsu/getbetter/getbetter/adapters/CaseRecordDownloadAdapter.java
@@ -0,0 +1,157 @@
+package com.dlsu.getbetter.getbetter.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.dlsu.getbetter.getbetter.R;
+import com.dlsu.getbetter.getbetter.objects.CaseRecord;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mikedayupay on 14/05/2016.
+ */
+public class CaseRecordDownloadAdapter extends ArrayAdapter<CaseRecord>{
+
+    private ArrayList<CaseRecord> caseRecordsList;
+    private LayoutInflater inflater;
+
+    public CaseRecordDownloadAdapter(Context context, int textViewResourceId, ArrayList<CaseRecord> objects) {
+        super(context, textViewResourceId, objects);
+
+        inflater = LayoutInflater.from(context);
+    }
+
+    private class ViewHolder {
+
+        TextView controlNumber;
+        TextView patientName;
+        TextView complaint;
+        TextView healthCenter;
+        TextView caseStatus;
+        TextView dateUpdated;
+        CheckBox checkBox;
+
+        public ViewHolder() {
+
+        }
+
+        public ViewHolder(TextView controlNumber, TextView patientName,
+                          TextView complaint, TextView healthCenter, TextView caseStatus, TextView dateUpdated, CheckBox checkBox) {
+
+            this.controlNumber = controlNumber;
+            this.patientName = patientName;
+            this.complaint = complaint;
+            this.healthCenter = healthCenter;
+            this.caseStatus = caseStatus;
+            this.dateUpdated = dateUpdated;
+            this.checkBox = checkBox;
+
+        }
+
+        public TextView getControlNumber() {
+            return controlNumber;
+        }
+
+        public void setControlNumber(TextView controlNumber) {
+            this.controlNumber = controlNumber;
+        }
+
+        public TextView getPatientName() {
+            return patientName;
+        }
+
+        public void setPatientName(TextView patientName) {
+            this.patientName = patientName;
+        }
+
+        public TextView getComplaint() {
+            return complaint;
+        }
+
+        public void setComplaint(TextView complaint) {
+            this.complaint = complaint;
+        }
+
+        public TextView getHealthCenter() {
+            return healthCenter;
+        }
+
+        public void setHealthCenter(TextView healthCenter) {
+            this.healthCenter = healthCenter;
+        }
+
+        public TextView getCaseStatus() {
+            return caseStatus;
+        }
+
+        public void setCaseStatus(TextView caseStatus) {
+            this.caseStatus = caseStatus;
+        }
+
+        public TextView getDateUpdated() {
+            return dateUpdated;
+        }
+
+        public void setDateUpdated(TextView dateUpdated) {
+            this.dateUpdated = dateUpdated;
+        }
+
+        public CheckBox getCheckBox() {
+            return checkBox;
+        }
+
+        public void setCheckBox(CheckBox checkBox) {
+            this.checkBox = checkBox;
+        }
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder holder = null;
+        CaseRecord caseRecord = (CaseRecord)this.getItem(position);
+
+        if(convertView == null) {
+
+            convertView = inflater.inflate(R.layout.case_record_item_checkbox, parent, false);
+
+            holder = new ViewHolder();
+            holder.patientName = (TextView)convertView.findViewById(R.id.upload_case_record_patient_name);
+            holder.controlNumber = (TextView)convertView.findViewById(R.id.upload_control_number);
+            holder.complaint = (TextView)convertView.findViewById(R.id.upload_caserecord_chief_complaint);
+            holder.healthCenter = (TextView)convertView.findViewById(R.id.upload_caserecord_health_center);
+            holder.caseStatus = (TextView)convertView.findViewById(R.id.upload_caserecord_status);
+            holder.checkBox = (CheckBox)convertView.findViewById(R.id.upload_caserecord_checkbox);
+            convertView.setTag(holder);
+
+            holder.checkBox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    CheckBox cb = (CheckBox) v;
+                    CaseRecord caseRecord = (CaseRecord) cb.getTag();
+                    caseRecord.setChecked(cb.isChecked());
+                }
+            });
+
+        } else {
+            holder = (ViewHolder)convertView.getTag();
+        }
+
+
+        holder.controlNumber.setText(caseRecord.getCaseRecordId() + "");
+        holder.patientName.setText(caseRecord.getPatientName());
+        holder.complaint.setText(caseRecord.getCaseRecordComplaint());
+        holder.healthCenter.setText(caseRecord.getHealthCenter());
+        holder.caseStatus.setText(caseRecord.getCaseRecordStatus());
+        holder.checkBox.setChecked(caseRecord.isChecked());
+        holder.checkBox.setTag(caseRecord);
+
+        return convertView;
+    }
+}
