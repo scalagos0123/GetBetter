24a3dca8b7ab5a896f0be5a5a2aadbe9711348a6
Jan Michael Dayupay <mike.dayupay03@gmail.com>
2016-05-30 12:56:58 +0800

    New directory URLs

--- app/src/main/java/com/dlsu/getbetter/getbetter/DirectoryConstants.java
+++ app/src/main/java/com/dlsu/getbetter/getbetter/DirectoryConstants.java
@@ -13,5 +13,12 @@ public class DirectoryConstants {
 
     public static final String UPLOAD_CASE_RECORD_SERVER_SCRIPT_URL = "http://128.199.205.226/get_better/upload_case_record.php";
 
+    public static final String DOWNLOAD_CASE_RECORD_LIST_URL = "http://128.199.205.226/get_better/download_data.php";
+
+    public static final String UPLOAD_CASE_RECORD_IMAGE_ATTACHMENTS_SERVER_SCRIPT_URL = "http://128.199.205.226/get_better/upload_image_attachment.php";
+
+    public static final String UPLOAD_CASE_RECORD_VIDEO_ATTACHMENTS_SERVER_SCRIPT_URL = "http://128.199.205.226/get_better/upload_video_attachment.php";
+
+    public static final String UPLOAD_CASE_RECORD_HISTORY_SERVER_SCRIPT_URL = "http://128.199.205.226/get_better/upload_case_record_history.php";
 
 }
