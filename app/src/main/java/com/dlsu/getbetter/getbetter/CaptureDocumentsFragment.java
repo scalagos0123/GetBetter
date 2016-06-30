49dfb61cfe3f66782495e2a161c46bb297b4bceb
Jan Michael Dayupay <mike.dayupay03@gmail.com>
2016-06-30 12:32:25 +0800

    Fixed view image

--- app/src/main/java/com/dlsu/getbetter/getbetter/CaptureDocumentsFragment.java
+++ app/src/main/java/com/dlsu/getbetter/getbetter/CaptureDocumentsFragment.java
@@ -1,10 +1,16 @@
 package com.dlsu.getbetter.getbetter;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -14,6 +20,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -36,9 +43,6 @@ public class CaptureDocumentsFragment extends Fragment implements View.OnClickLi
     private ImageView captureBasicInfoBtn;
     private ImageView captureFamilyHistBtn;
     private ImageView captureChiefComplaintBtn;
-    private TextView viewBasicInfoImage;
-    private TextView viewFamilySocialImage;
-    private TextView viewChiefCompImage;
 
     private static final int REQUEST_IMAGE1 = 100;
     private static final int REQUEST_IMAGE2 = 200;
@@ -48,9 +52,18 @@ public class CaptureDocumentsFragment extends Fragment implements View.OnClickLi
     private static final String FAMILY_SOCIAL_HISTORY_FORM_TITLE = "familysocialhistoryform";
     private static final String CHIEF_COMPLAINT_FORM_TITLE = "chiefcomplaintform";
 
-    private String patientInfoImagePath = "";
-    private String familySocialHistoryImagePath = "";
-    private String chiefComplaintImagePath = "";
+    private String patientInfoImagePath;
+    private String familySocialHistoryImagePath;
+    private String chiefComplaintImagePath;
+
+    // Hold a reference to the current animator,
+    // so that it can be canceled mid-way.
+    private Animator mCurrentAnimator;
+
+    // The system "short" animation time duration, in milliseconds. This
+    // duration is ideal for subtle animations or animations that occur
+    // very frequently.
+    private int mShortAnimationDuration;
 
     NewPatientSessionManager newPatientSessionManager;
 
@@ -70,14 +83,14 @@ public class CaptureDocumentsFragment extends Fragment implements View.OnClickLi
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_capture_documents, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_capture_documents, container, false);
 
         captureBasicInfoBtn = (ImageView)rootView.findViewById(R.id.capture_docu_patient_info_image);
         captureFamilyHistBtn = (ImageView)rootView.findViewById(R.id.capture_docu_family_social_history_image);
         captureChiefComplaintBtn = (ImageView)rootView.findViewById(R.id.capture_docu_chief_complaint_image);
-        viewBasicInfoImage = (TextView)rootView.findViewById(R.id.capture_docu_view_patient_info_image);
-        viewFamilySocialImage = (TextView)rootView.findViewById(R.id.capture_docu_view_family_social_image);
-        viewChiefCompImage = (TextView)rootView.findViewById(R.id.capture_docu_view_chief_complaint_image);
+        final Button viewBasicInfoImage = (Button) rootView.findViewById(R.id.capture_docu_view_patient_info_image);
+        final Button viewFamilySocialImage = (Button) rootView.findViewById(R.id.capture_docu_view_family_social_image);
+        final Button viewChiefCompImage = (Button) rootView.findViewById(R.id.capture_docu_view_chief_complaint_image);
 
         captureBasicInfoBtn.setOnClickListener(this);
         captureFamilyHistBtn.setOnClickListener(this);
@@ -90,6 +103,43 @@ public class CaptureDocumentsFragment extends Fragment implements View.OnClickLi
         Button backBtn = (Button)rootView.findViewById(R.id.capture_document_fragment_back_btn);
 
         nextBtn.setOnClickListener(this);
+        viewBasicInfoImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(patientInfoImagePath == null) {
+                    //do nothing
+                    Toast.makeText(getContext(), "No image to view.", Toast.LENGTH_LONG).show();
+                }else {
+                    zoomImageFromThumb(viewBasicInfoImage, patientInfoImagePath, rootView);
+                }
+
+            }
+        });
+        viewFamilySocialImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(familySocialHistoryImagePath == null) {
+                    Toast.makeText(getContext(), "No image to view.", Toast.LENGTH_LONG).show();
+                }else {
+                    zoomImageFromThumb(viewFamilySocialImage, familySocialHistoryImagePath, rootView);
+                }
+
+            }
+        });
+        viewChiefCompImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(chiefComplaintImagePath == null) {
+                    Toast.makeText(getContext(), "No image to view.", Toast.LENGTH_LONG).show();
+                } else {
+                    zoomImageFromThumb(viewChiefCompImage, chiefComplaintImagePath, rootView);
+                }
+
+            }
+        });
 
         return rootView;
     }
@@ -236,16 +286,6 @@ public class CaptureDocumentsFragment extends Fragment implements View.OnClickLi
                 }
                 break;
 
-            case R.id.capture_docu_view_patient_info_image:
-                break;
-
-            case R.id.capture_docu_view_family_social_image:
-                break;
-
-            case R.id.capture_docu_view_chief_complaint_image:
-                break;
-
-
         }
 
     }
@@ -298,50 +338,164 @@ public class CaptureDocumentsFragment extends Fragment implements View.OnClickLi
         mImageView.setImageBitmap(bitmap);
     }
 
-//    private void editImageTitle (final int requestCode) {
+    private void zoomImageFromThumb(final View thumbView, String photoPath, View rootView) {
+
+        // If there's an animation in progress, cancel it
+        // immediately and proceed with this one.
+        if (mCurrentAnimator != null) {
+            mCurrentAnimator.cancel();
+        }
+
+        // Load the high-resolution "zoomed-in" image.
+        final ImageView expandedImageView = (ImageView)rootView.findViewById(
+                R.id.expanded_image);
+
+//        int targetW = expandedImageView.getWidth();
+//        int targetH = expandedImageView.getHeight();
 //
-//        AlertDialog.Builder builder = new AlertDialog.Builder(this.getContext());
-//        builder.setTitle("Image Filename");
+//        // Get the dimensions of the bitmap
+//        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+//        bmOptions.inJustDecodeBounds = true;
+//        BitmapFactory.decodeFile(photoPath, bmOptions);
+//        int photoW = bmOptions.outWidth;
+//        int photoH = bmOptions.outHeight;
 //
-//        // Set up the input
-//        final EditText input = new EditText(this.getContext());
+//        // Determine how much to scale down the image
+//        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
 //
-//        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-//        input.setInputType(InputType.TYPE_CLASS_TEXT);
-//        builder.setView(input);
-//
-//        // Set up the buttons
-//        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//
-//                if (requestCode == REQUEST_IMAGE1) {
-//
-//                    image1Title = input.getText().toString();
-//                    personalInfoImageTitle.setText(image1Title);
-//                    Log.d("debug", "working" + image1Title);
-//
-//                } else if (requestCode == REQUEST_IMAGE2) {
-//
-//                    image2Title = input.getText().toString();
-//                    familyHistoryImageTitle.setText(image2Title);
-//                    Log.d("debug", "working" + image2Title);
-//
-//                } else if (requestCode == REQUEST_IMAGE3) {
-//
-//                    image3Title = input.getText().toString();
-//                    chiefComplaintImageTitle.setText(image3Title);
-//                }
-//
-//            }
-//        });
-//        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                dialog.cancel();
-//            }
-//        });
-//
-//        builder.show();
-//    }
+//        // Decode the image file into a Bitmap sized to fill the View
+//        bmOptions.inJustDecodeBounds = false;
+//        bmOptions.inSampleSize = scaleFactor;
+//        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(photoPath);
+
+        expandedImageView.setImageBitmap(bitmap);
+
+        // Calculate the starting and ending bounds for the zoomed-in image.
+        // This step involves lots of math. Yay, math.
+        final Rect startBounds = new Rect();
+        final Rect finalBounds = new Rect();
+        final Point globalOffset = new Point();
+
+        // The start bounds are the global visible rectangle of the thumbnail,
+        // and the final bounds are the global visible rectangle of the container
+        // view. Also set the container view's offset as the origin for the
+        // bounds, since that's the origin for the positioning animation
+        // properties (X, Y).
+        thumbView.getGlobalVisibleRect(startBounds);
+        rootView.findViewById(R.id.container)
+                .getGlobalVisibleRect(finalBounds, globalOffset);
+        startBounds.offset(-globalOffset.x, -globalOffset.y);
+        finalBounds.offset(-globalOffset.x, -globalOffset.y);
+
+        // Adjust the start bounds to be the same aspect ratio as the final
+        // bounds using the "center crop" technique. This prevents undesirable
+        // stretching during the animation. Also calculate the start scaling
+        // factor (the end scaling factor is always 1.0).
+        float startScale;
+        if ((float) finalBounds.width() / finalBounds.height()
+                > (float) startBounds.width() / startBounds.height()) {
+            // Extend start bounds horizontally
+            startScale = (float) startBounds.height() / finalBounds.height();
+            float startWidth = startScale * finalBounds.width();
+            float deltaWidth = (startWidth - startBounds.width()) / 2;
+            startBounds.left -= deltaWidth;
+            startBounds.right += deltaWidth;
+        } else {
+            // Extend start bounds vertically
+            startScale = (float) startBounds.width() / finalBounds.width();
+            float startHeight = startScale * finalBounds.height();
+            float deltaHeight = (startHeight - startBounds.height()) / 2;
+            startBounds.top -= deltaHeight;
+            startBounds.bottom += deltaHeight;
+        }
+
+        // Hide the thumbnail and show the zoomed-in view. When the animation
+        // begins, it will position the zoomed-in view in the place of the
+        // thumbnail.
+        thumbView.setAlpha(0f);
+        expandedImageView.setVisibility(View.VISIBLE);
+
+        // Set the pivot point for SCALE_X and SCALE_Y transformations
+        // to the top-left corner of the zoomed-in view (the default
+        // is the center of the view).
+        expandedImageView.setPivotX(0f);
+        expandedImageView.setPivotY(0f);
+
+        // Construct and run the parallel animation of the four translation and
+        // scale properties (X, Y, SCALE_X, and SCALE_Y).
+        AnimatorSet set = new AnimatorSet();
+        set
+                .play(ObjectAnimator.ofFloat(expandedImageView, View.X,
+                        startBounds.left, finalBounds.left))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.Y,
+                        startBounds.top, finalBounds.top))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X,
+                        startScale, 1f)).with(ObjectAnimator.ofFloat(expandedImageView,
+                View.SCALE_Y, startScale, 1f));
+        set.setDuration(mShortAnimationDuration);
+        set.setInterpolator(new DecelerateInterpolator());
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mCurrentAnimator = null;
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                mCurrentAnimator = null;
+            }
+        });
+        set.start();
+        mCurrentAnimator = set;
+
+        // Upon clicking the zoomed-in image, it should zoom back down
+        // to the original bounds and show the thumbnail instead of
+        // the expanded image.
+        final float startScaleFinal = startScale;
+        expandedImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCurrentAnimator != null) {
+                    mCurrentAnimator.cancel();
+                }
+
+                // Animate the four positioning/sizing properties in parallel,
+                // back to their original values.
+                AnimatorSet set = new AnimatorSet();
+                set.play(ObjectAnimator
+                        .ofFloat(expandedImageView, View.X, startBounds.left))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.Y,startBounds.top))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.SCALE_X, startScaleFinal))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.SCALE_Y, startScaleFinal));
+                set.setDuration(mShortAnimationDuration);
+                set.setInterpolator(new DecelerateInterpolator());
+                set.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        thumbView.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        mCurrentAnimator = null;
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        thumbView.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        mCurrentAnimator = null;
+                    }
+                });
+                set.start();
+                mCurrentAnimator = set;
+            }
+        });
+    }
+
 }
