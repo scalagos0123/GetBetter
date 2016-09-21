a5cbc437b7fecc72dda9833c14e262d952b2b868
Jan Michael Dayupay <mike.dayupay03@gmail.com>
2016-09-21 18:05:35 +0800

    Fix branded launch logo

--- app/src/main/java/com/dlsu/getbetter/getbetter/activities/HomeActivity.java
+++ app/src/main/java/com/dlsu/getbetter/getbetter/activities/HomeActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.balysv.materialripple.MaterialRippleLayout;
 import com.dlsu.getbetter.getbetter.AddInstructionsCaseFragment;
 import com.dlsu.getbetter.getbetter.ClosedCaseFragment;
 import com.dlsu.getbetter.getbetter.DetailsFragment;
